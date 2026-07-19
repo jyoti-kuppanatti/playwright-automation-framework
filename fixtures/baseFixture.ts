0be8eeb0c1c90ea8e284f38c79eb46559702e0f7
Jyoti <jyotisarade@gmail.com>
2026-07-19 23:01:35 +0530

    feat: implement Playwright custom fixtures

---
+++ fixtures/baseFixture.ts
@@ -0,0 +1,23 @@
+import {test as base, expect} from '@playwright/test'
+import { LoginPage } from '../pages/LoginPage'
+import { DashboardPage } from '../pages/DashboardPage'
+
+type Fixtures  = {
+    loginPage : LoginPage,
+    dashboardPage : DashboardPage
+}
+
+export const test = base.extend<Fixtures>({
+    loginPage: async ({page}, use)=>{
+        const loginPage = new LoginPage(page); 
+        await use(loginPage);
+},
+
+   dashboardPage: async ({ page }, use) => {
+        const dashboardPage = new DashboardPage(page);
+        await use(dashboardPage);
+    }
+    });
+
+    export { expect };
+
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,10 @@
-import { test } from '@playwright/test';
+import { test } from '../fixtures/baseFixture';
 import { LoginPage } from '../pages/LoginPage';
 import { DashboardPage } from '../pages/DashboardPage';
 import users from '../test-data/users.json';
 
 
-test('Verify user login successfully', async ({ page }) => {
-
-    const loginPage = new LoginPage(page);
-    const dashboardPage = new DashboardPage(page);
+test('Verify user login successfully', async ({ page, loginPage, dashboardPage}) => {
 
     await loginPage.navigate('/web/index.php/auth/login');
 
